93066737c0a310f4aed1c7de653613cd4e09bd45
Bob Schreiner <bobschreiner@Bobs-MacBook-Pro.local>
2026-05-11 14:56:29 +0300

    Changed arborx cmake

--- cmake/nukexc-arborx.cmake
+++ cmake/nukexc-arborx.cmake
@@ -1,11 +1,16 @@
-find_package(ArborX REQUIRED) # Try to find Kokkos externally
+find_package(ArborX QUIET)
 
 if(ArborX_FOUND)
-	
-	message(STATUS "Found ArborX: ${ArborX_DIR} (version \"${ArborX_VERSION}\")")
-
+    message(STATUS "Found ArborX: ${ArborX_DIR} (version \"${ArborX_VERSION}\")")
 else()
-	message(STATUS "ArborX not found externally. Please install ArborX first.")
+    message(STATUS "ArborX not found externally, fetching...")
+    include(FetchContent)
+    FetchContent_Declare(
+        ArborX
+        GIT_REPOSITORY https://github.com/arborx/ArborX.git
+        GIT_TAG        v2.0.1
+    )
+    FetchContent_MakeAvailable(ArborX)
 endif()
 
 target_link_libraries(libnukexc INTERFACE ArborX::ArborX)
