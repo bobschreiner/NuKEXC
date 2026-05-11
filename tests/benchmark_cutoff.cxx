eb7f476ce1da2a80e43816762e7ef3249a1e70ed
Bob Schreiner <bobschreiner@Bobs-MacBook-Pro.local>
2026-05-11 15:01:06 +0300

    Removed include Kokkos check usage

--- tests/benchmark_cutoff.cxx
+++ tests/benchmark_cutoff.cxx
@@ -19,9 +19,9 @@
  */
 
 #include <Kokkos_Core.hpp>
+
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <impl/Kokkos_CheckUsage.hpp>
 #include <integratorxx/quadratures/radial/treutlerahlrichs.hpp>
 #include <iomanip>
 #include <iostream>
